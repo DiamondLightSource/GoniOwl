5474288eb65a329c4ae852fb9f2fb96fffbf831a
co2e14 <christian.orr@diamond.ac.uk>
2025-09-03 16:02:13 +0100

    remove old non binary way of doing things

--- src/GoniOwl/GoniOwl_controller.py
+++ src/GoniOwl/GoniOwl_controller.py
@@ -7,25 +7,38 @@ from io import BytesIO
 import urllib
 from time import sleep
 import numpy as np
+import datetime
+
 
 try:
     import epicscorelibs.path.cothread
 except ImportError:
     pass
 
+# 20250714-111015_epoch20_binary_batch4.h5 is 1/5 scaled image! 
+
+image_divider = 5
 
 class GoniOwlController:
     def __init__(self):
-        self.model = tf.keras.models.load_model("./categorical_11072024.h5")
+        self.log_path = "/dls/science/groups/i23/scripts/chris/GoniOwl/src/GoniOwl/logs/GoniOwl_binary_controller.log"
+        with open(self.log_path, "a") as f:
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            f.write(f"{timestamp} - INFO - GoniOwl binary controller started.\n")
+        self.model_name = "20250714-111015_epoch20_binary_batch4.h5"
+        self.model = tf.keras.models.load_model(f"./{self.model_name}")
+        with open(self.log_path, "a") as f:
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            f.write(f"{timestamp} - INFO - Model {self.model_name} loaded successfully.\n")
         self.model.summary()
-        self.classes = ["dark", "light", "pinoff", "pinon"]
+        self.classes = ["pinoff", "pinon"]
+
 
     def urltoimage(self, url):
         self.resp = urllib.request.urlopen(url)
         self.image = np.asarray(bytearray(self.resp.read()), dtype="uint8")
         self.image = cv2.imdecode(self.image, cv2.IMREAD_COLOR)
-        self.image = self.image[100:900, 100:900]
-#        self.image = self.image[400:700, 610:770]
+        self.image = self.image[100:-100, 100:-100]
         cv2.imwrite("tmp.jpg", self.image)
         _, buffer = cv2.imencode(".jpg", self.image)
         io_buf = BytesIO(buffer)
@@ -35,18 +48,41 @@ class GoniOwlController:
         self.stream = self.urltoimage(
             "http://bl23i-di-serv-04.diamond.ac.uk:8080/ECAM6.mjpg.jpg"
         )
+        self.stream.seek(0)
+        img_bytes = self.stream.read()
+        img_tensor = tf.image.decode_image(img_bytes, channels=3)
+        img_height, img_width = int(img_tensor.shape[0]), int(img_tensor.shape[1])
+        self.stream.seek(0)
         self.img_in = keras.preprocessing.image.load_img(
-            (self.stream), target_size=(800, 800)
+            (self.stream), target_size=(int(img_height / image_divider), int(img_width / image_divider))
         )
         self.img_array = keras.preprocessing.image.img_to_array(self.img_in)
         self.img_array = tf.expand_dims(self.img_array, 0)
         self.predictions = self.model.predict(self.img_array, verbose=0)
-        self.score = tf.nn.softmax(self.predictions[0])
-        print(np.argmax(self.score))
-        # print("This image most likely belongs to {} with a {:.2f} percent confidence.".format(classes[np.argmax(score)], 100 * np.max(score)))
-        print(
-            "Status is probably {} with {:.2f} % conf.".format(
-                self.classes[np.argmax(self.score)], 200 * np.max(self.score)
-            )
-        )
-        return np.argmax(self.score)
+        self.score = self.predictions[0]
+
+        if self.score < 0.15:
+            print(f"{self.score} is {self.classes[0]}")
+            self.predicted_label = self.classes[0]
+            self.score = 1 - self.score
+            self.printscore()
+            return 1
+        elif self.score > 0.85:
+            print(f"{self.score} is {self.classes[1]}")
+            self.predicted_label = self.classes[1]
+            self.printscore()
+            return 2
+        else:
+            print(f"{self.score} is unknown")
+            self.predicted_label = "unknown"
+            self.printscore()
+            return 3
+        
+    def printscore(self):
+        rounded_score = np.round((self.score * 100), 3)
+        msg = f"Status is {self.predicted_label} with {str(rounded_score)} % conf."
+        print(msg)
+        
+        with open(self.log_path, "a") as f:
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            f.write(f"{timestamp} - INFO - {msg}\n")
